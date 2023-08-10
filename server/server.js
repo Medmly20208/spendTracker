0ad3fd11ef8f42441a8c649cfff9ec63625304a0
medmly20208 <moulaymohamed856@gmail.com>
2023-08-10 22:51:56 +0100

    add origin

--- server/server.js
+++ server/server.js
@@ -17,10 +17,7 @@ const server = app.listen(PORT, () => {
 const io = require("socket.io")(server, {
   pingTimeout: 60000,
   cors: {
-    origin: [
-      "http://localhost:5173",
-      "https://spend-tracker-2gpi-bljwkuiui-medmly20208.vercel.app",
-    ],
+    origin: "*",
   },
 });
 
